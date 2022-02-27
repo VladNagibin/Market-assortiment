1acccba10f7086f417a749ead289c0d3b03b8b56
VladNagibin <56471040+VladNagibin@users.noreply.github.com>
2022-02-27 09:48:28 +0300

    1

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHttp } from '../hooks/http.hook'
+import { useMessage } from '../hooks/message.hook'
 
 export default function Main() {
-  
+  const {loading,request,error,CleanErrors}=useHttp()
+  const message = useMessage()
+  const [categories,updateCategories] = useState([])
+  const clickCategories = event =>{
+
+  }
+  useEffect(()=>{
+    //console.log(error)
+    message(error)
+    CleanErrors()
+  },[error,CleanErrors])
   return (
     <div>
       <h1>Мейн</h1>
