5dca2b232e00fcc5fe30462cf75be1d3a6e9d370
VladNagibin <56471040+VladNagibin@users.noreply.github.com>
2022-04-08 13:01:05 +0300

    деплой

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ app.use(express.json({extended:true}))
 //app.use(express.static(path.join(__dirname,'public')))
 app.use('/api',Router)
 app.use(Cookies)
-
+if(process.env.NODE_ENV ==='production'){
+    app.use('/',express.static(path.join(__dirname,'client','build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 async function start(){
     try{
         await mongoose.connect('mongodb+srv://Vlad:123@cluster0.zy9vv.mongodb.net/Database')
