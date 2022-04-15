ea0b47c0423fb772aa5bd71375316bbd9e9f4bcb
VladNagibin <56471040+VladNagibin@users.noreply.github.com>
2022-04-15 14:57:39 +0300

    верстка товара начало

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -10,19 +10,24 @@ export default function Detail() {
     barcode: '',
     min_quantity: 0,
     image: '',
-    images: []
+    images: [],
+    parameters:[],
+    description:''
   })
   const productId = useParams().id
   async function getProduct() {
     const dataAll = await request('/api/product?id=' + productId)
     const data = dataAll.result
+    console.log(data.parameters.length)
     setProduct({
       name: data.name,
       price: data.price,
       barcode: data.barcode,
       min_quantity: data.min_quantity,
       image: data.image,
-      images: data.images
+      images: [...data.images,data.image],
+      parameters:data.parameters,
+      description:data.description
     })
   }
   useState(() => {
@@ -31,8 +36,13 @@ export default function Detail() {
   return (
     <div className='container'>
       <div className='row'>
-
-        <div className="card large">
+        <div className='barcode-detail valign-wrapper col s2'>
+          <span className='barcode-detail-text'>{product.barcode}</span>
+        </div>
+        <div className='name-detail col s7' >
+          <span>{product.name}</span>
+        </div>
+        {/* <div className="card large">
           <div className="card-image">
             <img src={product.image} alt='product' />
             <span className="card-title">{product.name}</span>
@@ -44,6 +54,33 @@ export default function Detail() {
             <button className='waves-effect waves-light btn' href="#">Купить</button>
           </div>
 
+        </div> */}
+      </div>
+      <div className='row'>
+        <div className='col s2'>
+          {product.images.map(elem=>{
+            return (<img key={elem} className='img-detail' src={elem}/>)
+          })}
+        </div>
+        <div className='col s5'>
+          <img src={product.image}/>
+        </div>
+        <div className='col s5 stats-detail'>
+          <span>Технические характеристики</span>
+          {product.parameters.map(elem=>{
+            return(<div key={elem}><br/><span>{elem}</span></div>)
+          })
+
+          }
+        </div>
+      </div>
+      <div className='row detail-desc'>
+        <h5>Описание</h5>
+        <div className='col s6'>
+          <span>{product.description}</span>
+        </div>
+        <div className='col s6 detail-desc-info'>
+          <span>Товары представлены в ассортименте, выбор цветов или моделей не предоставляется. На фотографиях могут быть представлены не все варианты.</span>
         </div>
       </div>
     </div>
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ if(process.env.NODE_ENV ==='production'){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
+
 async function start(){
     try{
         await mongoose.connect('mongodb+srv://Vlad:123@cluster0.zy9vv.mongodb.net/Database')
